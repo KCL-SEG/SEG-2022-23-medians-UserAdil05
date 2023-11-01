6d24086604917a8dfebcdabb611f8bddefd170c6
k20056435@kcl.ac.uk <k20056435@kcl.ac.uk>
2023-11-01 18:25:12 +0000

    median calcautio

--- median.py
+++ median.py
@@ -10,3 +10,16 @@ while True:
     else:
         break
 print(numbers)
+
+
+
+numbers.sort() 
+
+
+n = len(numbers)
+if n % 2 == 0:
+    median = (numbers[n // 2 - 1] + numbers[n // 2]) / 2
+else:
+    median = numbers[n // 2]
+
+print("Median:", median)
